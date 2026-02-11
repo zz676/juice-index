7b039658f6193d20dc087218be2417e5ae35d3c8
Zhisheng Zhou <zhizhou@linkedin.com>
2026-02-10 20:55:29 -0800

    Update Stripe API version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
+  apiVersion: "2025-02-24.acacia",
 });
 
 export type Plan = "starter" | "pro";
